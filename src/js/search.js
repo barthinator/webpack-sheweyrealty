7a29f7cfbec0079988c30fa82a3b624eb44ceeb3
David B <eldor@Davids-iMac-2.local>
2018-05-16 09:25:06 -0700

    Updates to new webpack

--- src/js/search.js
+++ src/js/search.js
@@ -9,6 +9,8 @@ const Search = () => (
     <a className="rent-link" href="https://rentalmgmtinc.appfolio.com/connect"> Pay Rent </a>
     <h1>Find Rental Properties</h1>
     <iframe className="listings" src="http://rentalmgmtinc.appfolio.com/listings?1514432888282&amp;theme_color=%23676767"> </iframe>
+    <h1>Search</h1>
+    <iframe className="listings" src="http://www.crmls.org/servlet/lDisplayListings?LA=EN"> </iframe>
   </div>
 
   </div>
